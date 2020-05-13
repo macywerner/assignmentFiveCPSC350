6db834f398585c939a9f44178266e141ee3603c1
Macy Werner <49294194+macywerner@users.noreply.github.com>
2020-05-12 22:30:02 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,38 +8,6 @@ using namespace std;
 
 int main(int argc, char** argv){
   
-  //check for existing tables and deserialize if so
-  /*
-  //for faculty
-  ifstream inFSOne;
-  BST<Faculty>* masterFaculty = new BST<Faculty>();
-
-  inFSOne.open("facultyTable.txt");
-
-  if(!inFSOne.is_open()){
-    cout << "Could not open file. Creating facultyTable.txt" << endl;
-  }
-  else{
-    masterFaculty->deSerialize(inFSOne);
-  }
-  
-  //for student
-  ifstream inFSTwo;
-  BST<Student>* masterStudent = new BST<Student>();
-
-  inFSTwo.open("studentTable.txt");
-
-  if(!inFSTwo.is_open()){
-    cout << "Could not open file. Creating studentTable.txt" << endl;
-  }
-  else{
-    masterStudent->deSerialize(inFSTwo);
-  }
-  
-  delete masterFaculty;
-  delete masterStudent;
-  */
-  
   Database *db = new Database();
   
   db->mainLoop();
